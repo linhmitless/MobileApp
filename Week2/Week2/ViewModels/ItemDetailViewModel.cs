03a015ec0d9bf474cbbc60a8151e02b9d048681b
Linh Y. Nguyen <nguye468@seattleu.edu>
2019-01-17 23:54:16 -0800

    Add project files.

---
+++ Week2/Week2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Week2.Models;
+
+namespace Week2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Week2/Week2/Views/InfoPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Week2.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InfoPage : ContentPage
+	{
+		public InfoPage ()
+		{
+			InitializeComponent ();
+        }
+        async void CharacterPageButtonClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CharacterPage());
+        }
+
+        async void ScorePageButtonClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ScorePage());
+        }
+        async void ItemsPageButtonClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ItemsPage());
+        }
+	}
+}
